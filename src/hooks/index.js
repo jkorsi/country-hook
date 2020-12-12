aafd515ebf2855538922b028d950ddb0c219146b
Juho Suni <juho.suni@protonmail.com>
2020-12-12 16:44:02 +0200

    Task 7.7 done

---
+++ src/hooks/index.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+export const useField = (type) =>
+{
+
+  const [value, setValue] = useState('')
+
+  const onChange = (event) =>
+  {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+export const useCountry = (name) =>
+{
+  const [country, setCountryData] = useState(null)
+
+  const apiLink = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+
+  //HOOK AND EFFECT-----------------------------
+  const hook = () =>
+  {
+    if (name) { 
+    axios
+      .get(apiLink)
+      .then(response =>
+      {
+        const responseData = response.data[0]
+        const found = response.status === 200 ? true : false
+        const countryToSet = {
+          ...responseData,
+          found: found
+        }
+        setCountryData(countryToSet)
+      })
+    }
+  }
+  useEffect(hook, [name, apiLink])
+  //---------------------------------------------
+
+  return country
+}
